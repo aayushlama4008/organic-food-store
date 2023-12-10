27693b962bd97761d98a4ffd6f830a5657aaaab7
aayushlama4008 <aayushlama4008@gmail.com>
2023-12-10 03:08:56 +0100

    resolved

--- src/components/Discount/Discount.jsx
+++ src/components/Discount/Discount.jsx
@@ -1,21 +1,20 @@
-import { Link } from 'react-router-dom'
-import icons from '../../constants/icons'
-import './Discount.scss'
-
+import { Link } from "react-router-dom";
+import icons from "../../constants/icons";
+import "./newDiscount.scss";
 
 const Discount = () => {
   return (
     <div className="app__discount">
-        <h1 className='semi-title-text ' style={{color:'white'}}>
+      <h1 className="semi-title-text " style={{ color: "white" }}>
         Get 25% Off On Your First Purchase!
-        </h1>
-        <Link to={"/Shop"} className='discount-button'>
-            <button className="button ">
-              <icons.cart size={21} /> SHOP NOW
-            </button>
-          </Link>
+      </h1>
+      <Link to={"/Shop"} className="discount-button">
+        <button className="button ">
+          <icons.cart size={21} /> SHOP NOW
+        </button>
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default Discount
+export default Discount;
