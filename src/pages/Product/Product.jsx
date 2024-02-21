4893419ac78bee7a89d3d3e1d579062f6db7862b
aayushlama4008 <aayushlama4008@gmail.com>
2024-02-21 01:57:43 +0100

    Cleared the product page

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,15 +1,14 @@
 import React, { useContext } from "react";
-import "./product.scss";
+
 import { ShopContext } from "../../context/ShopContext";
 import { useParams } from "react-router-dom";
 import BreadCrums from "../../components/BreadCrums/BreadCrums";
 import ProductDisplay from "../../components/ProductDisplay/ProductDisplay";
 
-import "./product.scss";
 const Product = () => {
   const { allProducts } = useContext(ShopContext);
   const { productId } = useParams();
-  // console.log(allProducts.allProducts)
+
   const product = allProducts.allProducts.find(
     (p) => p.id === Number(productId)
   );
